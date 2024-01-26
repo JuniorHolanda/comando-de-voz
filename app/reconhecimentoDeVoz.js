e4b12e8caee66888c8e2f41d470eb87d332725d8
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-01-25 23:08:32 -0300

    correção
    corrigi o nome da função

--- app/reconhecimentoDeVoz.js
+++ app/reconhecimentoDeVoz.js
@@ -10,12 +10,12 @@ recognition.addEventListener('result', onSpeak);
 
 function onSpeak (e) {
     chute = e.results[0][0].transcript
-    exibeChuteNaTela('chute')
+    exibeChuteNaTela(chute)
 }
 
-function exibeChutenaTela (chute) {
+function exibeChuteNaTela (chute) {
     elementoChute.innerHTML = `
-        <div>Voce disse </div>
+        <div>Voce disse:</div>
         <span class="box">${chute}</span>
     `
 }
