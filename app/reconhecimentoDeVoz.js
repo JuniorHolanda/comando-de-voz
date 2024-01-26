c976a9ece43e280fcb1a751fb57d1dd76f7a105a
Júnor Holanda <139183744+JuniorHolanda@users.noreply.github.com>
2024-01-25 23:03:58 -0300

    manipulando o DOM
    agora estou manipulando o DOM para que o audio seja transcrito na tela para o usuário

---
+++ app/reconhecimentoDeVoz.js
@@ -0,0 +1,21 @@
+const elementoChute = document.querySelector('#chute')
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.leng = 'pt-Br';
+recognition.start()
+
+recognition.addEventListener('result', onSpeak);
+
+function onSpeak (e) {
+    chute = e.results[0][0].transcript
+    exibeChuteNaTela('chute')
+}
+
+function exibeChutenaTela (chute) {
+    elementoChute.innerHTML = `
+        <div>Voce disse </div>
+        <span class="box">${chute}</span>
+    `
+}
